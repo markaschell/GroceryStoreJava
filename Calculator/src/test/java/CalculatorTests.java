f71d7bb3c52b8a728b20ec3a1ee7f19d3aafa50f
markaschell <mark.a.schell@gmail.com>
2021-01-30 10:25:57 -0500

    First Test

--- Calculator/src/test/java/CalculatorTests.java
+++ Calculator/src/test/java/CalculatorTests.java
@@ -1,2 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+
 public class CalculatorTests {
+
+    Calculator _calculator;
+
+    @BeforeEach
+    void setup()
+    {
+        _calculator = new Calculator();
+    }
+
+    @Test
+    void Calculate_OneApple()
+    {
+        Double price = _calculator.CalculatePrice(new HashMap<String, Integer>() {{ put("apple", 1); }});
+
+        Assertions.assertEquals(0.1, price);
+    }
 }
